51c8f4b1afe92e0f850d7f2f3c583ab7db94735a
qore23 <ing.tolosa@gmail.com>
2022-09-08 10:36:52 -0500

    mascota

---
+++ MascotaFeliz.App.Persistencia/AppRepositorios/IRepositorioMascota.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MascotaFeliz.App.Dominio;
+namespace MascotaFeliz.App.Persistencia
+{
+    public interface IRepositorioMascota
+    {
+        Mascota AddMascota(Mascota mascota);
+    }
+}
---
+++ MascotaFeliz.App.Persistencia/AppRepositorios/RepositorioMascota.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MascotaFeliz.App.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace MascotaFeliz.App.Persistencia
+{
+    public class RepositorioMascota:IRepositorioMascota
+    {
+        private readonly AppContext _appContext;
+
+        public RepositorioMascota(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        public Mascota AddMascota(Mascota mascota)
+        {
+            var mascotaAdicionado = _appContext.Mascotas.Add(mascota);
+            _appContext.SaveChanges();
+            return mascotaAdicionado.Entity;
+        }
+    }
+}
