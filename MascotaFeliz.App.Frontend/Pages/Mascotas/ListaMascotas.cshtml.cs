5ac0c261a21118c42c95f6a4e97f09ecd20f7b47
qore23 <ing.tolosa@gmail.com>
2022-09-21 14:57:43 -0500

    al dia

--- MascotaFeliz.App.Frontend/Pages/Mascotas/ListaMascotas.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Mascotas/ListaMascotas.cshtml.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
 
 namespace MascotaFeliz.App.Frontend.Pages
 {
     public class ListaMascotasModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+        
     }
 }
