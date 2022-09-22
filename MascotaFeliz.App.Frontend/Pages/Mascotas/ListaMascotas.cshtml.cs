d1532ef85e9fa3a3c19a5f1ef722ac33f1f14596
qore23 <ing.tolosa@gmail.com>
2022-09-22 10:27:21 -0500

    listar mascotas ok

--- MascotaFeliz.App.Frontend/Pages/Mascotas/ListaMascotas.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Mascotas/ListaMascotas.cshtml.cs
@@ -11,6 +11,18 @@ namespace MascotaFeliz.App.Frontend.Pages
 {
     public class ListaMascotasModel : PageModel
     {
+        private readonly IRepositorioMascota _repoMascota;
         
+        public IEnumerable<Mascota> listaMascotas {get;set;} 
+
+        public ListaMascotasModel()
+        {
+            this._repoMascota = new RepositorioMascota(new Persistencia.AppContext());
+        }
+        
+        public void OnGet()
+        {
+            listaMascotas = _repoMascota.GetAllMascotas();
+        }
     }
 }
