316a992801e7cda5d8eab4e63d8448949e811f21
qore23 <ing.tolosa@gmail.com>
2022-09-22 14:21:10 -0500

    interfaces repositorio mascota

--- MascotaFeliz.App.Persistencia/AppRepositorios/IRepositorioMascota.cs
+++ MascotaFeliz.App.Persistencia/AppRepositorios/IRepositorioMascota.cs
@@ -13,5 +13,9 @@ namespace MascotaFeliz.App.Persistencia
         void DeleteMascota(int idMascota);
         Mascota GetMascota(int idMascota);
         IEnumerable<Mascota> GetMascotasPorFiltro(string filtro); //buscar por filtro  
+
+        Veterinario AsignarVeterinario(int idMascota, int idVeterinario);//asinar veterinario a mascota
+        Dueno AsignarDueno(int idMascota, int idDueno);// asignar dueño a mascota
+        Historia AsignarHistoria(int idMascota, int idHistoria);// asignar historia a mascota
     }
 }
