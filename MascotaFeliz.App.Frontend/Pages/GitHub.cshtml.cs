d2a0f8668e528df0f6116d82f1365eaf350c6070
qore23 <ing.tolosa@gmail.com>
2022-10-01 17:22:26 -0500

    fin

---
+++ MascotaFeliz.App.Frontend/Pages/GitHub.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GlobalCar.App.Frondend.Pages
+{
+    public class GitHubModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
